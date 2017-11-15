ac4bca1db3e4def19bca5e8ced037c6d344f550d
Josh Lehne <jdlehne@gmail.com>
2017-11-15 00:08:05 -0600

    fixing branch merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card/Card";
+import PlayedCards from "./components/PlayedCards/PlayedCards";
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Card />
+        <PlayedCards />
       </div>
     );
   }
