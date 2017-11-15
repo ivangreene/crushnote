c670c253a60ab01d818ab29169b3f1c8a53e4886
Josh Lehne <jdlehne@gmail.com>
2017-11-15 16:12:01 -0600

    rough 2v2 layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import './App.css';
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import Card from "./components/Card/Card";
   //  <PlayedCards cards={data} />
 import CardList from "./components/CardList/CardList";
-import PlayedCards from "./components/PlayedCards/PlayedCards";
+//import PlayedCards from "./components/PlayedCards/PlayedCards";
+import GameView from "./pages/GameView";
 
 let data = require('./cards.json');
 
@@ -12,11 +13,13 @@ class App extends Component {
     return (
       <div className="App">
       <div>
+            <GameView />
         <CardList cards={data} />
 
       </div>
     </div>);
   }
 }
+
 
 export default App;
---
+++ client/src/pages/GameView.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+//import CardList from "../components/CardList/CardList";
+import Card from "../components/Card/Card";
+//import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import "./GameView.css";
+
+let data = require('../cards.json');
+
+class GameView extends Component {
+  state = {};
+
+  render() {
+    return (<div>
+      <div className="navbar">
+        <h3>Game Nav? what info/components to go here?</h3>
+      </div>
+
+      <div id="card_view">
+        <div id="oppenent1_hand">
+          <p>Oppenent hand</p>
+          <Card card={data[1]}/>
+        </div>
+
+        <div id="player_hand">
+          <p>Player Hand</p>
+          <Card card={data[3]}/>
+        </div>
+
+        <div id="cards_in_play">
+          <p>Cards in play</p>
+          <Card card="card" ={data[2]}/>
+        </div>
+      </div>
+
+      <div className="hud">
+        <h3>Player buttons/interaction which components for here??</h3>
+      </div>
+      <h2>Bottom of GameView - CardList Ref below</h2>
+    </div>);
+  }
+
+}
+
+export default GameView;
