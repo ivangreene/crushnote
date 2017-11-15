1e97ec7418b34f34728f9108c50bcfb2f3183c8f
Josh Lehne <jdlehne@gmail.com>
2017-11-15 00:09:50 -0600

    fixing branch merge

---
+++ client/src/components/PlayedCards/PlayedCards.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./PlayedCards.css";
+
+const PlayedCards = ({}) => {
+  //const video = props.video; makes the above ({video}) available instead of (props) and then the const declaration
+
+    return (
+      <div className="container">
+      <ul>
+        <li>generated card</li>
+        <li>generated card</li>
+        <li>generated card</li>
+        <li>generated card</li>
+        <li>generated card</li>
+
+        <li>generated card</li>
+        <li>generated card</li>
+        <li>generated card</li>
+
+      </ul>
+    </div>
+    );
+}
+
+export default PlayedCards;
