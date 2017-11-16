34b9778f8792017fc108b51bf6c310f6e6738cdc
Hillary Spratt <spratth@gmail.com>
2017-11-16 09:56:45 -0600

    Updated mongoose call logic in gamesController.js file and moved some of the code out of the controller into a lib file called gamesDbCalls

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,35 +1,35 @@
-const db = require("../models");
+const callDb = require("../lib/gamesDbCalls");
 
 module.exports = {
+  // lists all current games: for joining/watching games
   findAll: function(req, res) {
-    db.Game.find(req.query)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+    callDb.findAllPromise(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
+  // get game matching your selected game id
+  // returned as part of auth cookie info for each user
   findById: function(req, res) {
-    db.Game
-      .findById(req.params.id)
+    callDb.findByIdPromise(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // make a new game
   create: function(req, res) {
-    db.Game
-      .create(req.body)
+    callDb.createPromise(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // run this to play the game every turn or socket.io update cycle
   update: function(req, res) {
-    db.Game
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+    callDb.updatePromise(req.params.id, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // when last round ends or last player leaves game room, remove game
   remove: function(req, res) {
-    db.Game
-      .findById({ _id: req.params.id })
-      .then(dbModel => {
-        dbModel.remove();
-      }).then(() => res.json({success: true}))
+    callDb.removePromise(req.params.id)
+      .then(() => res.json({success: true}))
       .catch(err => res.status(422).json(err));
   }
 };
