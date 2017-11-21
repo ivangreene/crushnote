8d0f813feffff01c4062420dc7539f3ae1b34dd3
Ivan Greene <ivan@ivan.sh>
2017-11-21 13:20:05 -0600

    move sockets into own file, cleanup server.js, add colored console.log origin convention

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({
   silent: process.env.NODE_ENV === 'production'
 });
+const chalk = require('chalk');
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -10,6 +11,7 @@ const MongoStore = require('connect-mongo')(session);
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
@@ -23,7 +25,9 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   store: new MongoStore({mongooseConnection: mongoose.connection})
 });
+// Session for express app
 app.use(sessionMiddleware);
+// Session for socket.io
 io.use((socket, next) => sessionMiddleware(socket.request, socket.request.res, next));
 
 // Serve up static assets
@@ -36,27 +40,14 @@ require('./routes/socket')(io);
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/" + process.env.LOCAL_MONGO_DB, {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/" + process.env.LOCAL_MONGO_DB, {useMongoClient: true})
+  .then(() => console.log(`${chalk.underline.yellow(`Mongoose`)}: Connected to MongoDB server.`))
+  .catch(err => {
+    console.error(`${chalk.underline.yellow(`Mongoose`)}: Connection error: ${err}`);
+    process.exit(1);
+  });
 
 // Start the API server
-server.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
-
-//-----Socket config------//
-io.on('connection', (socket) => {
-  console.log("User connected");
-  console.log(socket.id);
-
-  socket.on('SEND_MESSAGE', function(data) {
-    io.emit('RECEIVE_MESSAGE', data);
-  });
-
-  socket.on('error', function(err) {
-    console.log(err);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  });
+server.listen(PORT, HOST, function() {
+  console.log(`${chalk.underline.white(`express`)}: 🌎  ==> API Server now listening at ${HOST}:${PORT}`);
 });
