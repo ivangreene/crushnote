bb457797e73638265fdebf006293dd734f99fe70
Hillary Spratt <spratth@gmail.com>
2017-12-02 19:32:07 -0600

    Temporary fix for /main page: comment out two variables that don't get passed to the client side yet and were preventing the page from loading.

--- client/src/components/LobbyComponents/PlayerListDetail.js
+++ client/src/components/LobbyComponents/PlayerListDetail.js
@@ -6,7 +6,9 @@ const PlayerListDetail = ({player}) => {
 
   return (<div className="player_info">
     <p id="player_name">{player.username}</p>
-    <p id="player_record">Wins: {player.stats.wins} / Losses: {player.stats.losses}</p>
+	{/* player.stats doesn't exist and using it prevents the page from loading */}
+	{/* commenting these variables out until database calls work correctly */}
+    <p id="player_record">Wins: 1{/*player.stats.wins*/} / Losses: 0{/*player.stats.losses*/}</p>
     <br></br>
     <button className="green">Start Game</button>
   </div>);
