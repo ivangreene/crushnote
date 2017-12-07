542c7e561d39445f30cdc98e52fb23d2b1eaa1b2
Ivan Greene <ivan@ivan.sh>
2017-12-07 12:06:56 -0600

    add 2 day expiration for stale games, purge completed games every 2 hours

--- models/game.js
+++ models/game.js
@@ -1,7 +1,10 @@
 const mongoose = { Schema } = require('mongoose');
 const uniqueValidator = require("mongoose-unique-validator");
+const cron = require('node-cron');
+const chalk = require('chalk');
 
 const gameSchema = new Schema({
+  updatedAt: { type: Date, expires: '2d' },
   playerOrder: [
     {
       type: Schema.Types.ObjectId,
@@ -18,9 +21,15 @@ const gameSchema = new Schema({
   completed: Boolean,
   waiting: Boolean,
   winner: { type: Schema.Types.ObjectId, ref: 'User' }
-}, { minimize: false });
+}, { minimize: false, timestamps: true });
 
 const Game = mongoose.model("Game", gameSchema);
+
+cron.schedule('0 */2 * * *', () => {
+  Game.deleteMany({ completed: true })
+    .then(purge => console.log(`${chalk.underline.yellow(`Mongoose`)}: Purged ${purge.deletedCount} completed games.`))
+    .catch(console.error);
+});
 
 gameSchema.plugin(uniqueValidator, { message: 'Error, expected {PATH} to be unique.' });
 
